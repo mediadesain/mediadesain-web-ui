7198c03a7b68f96f755f57e3d43284b1937ec145
Reynaldi Julio Setiabudi <reynaldi.setiabudi@jublia.com>
2021-09-18 16:29:38 +0700

    FEATURE(my-project) : My project submition form and admin reviewer section added

--- src/app/pages/my-account/my-account.module.ts
+++ src/app/pages/my-account/my-account.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { MyAccountRoutingModule } from './my-account-routing.module';
 import { MyAccountComponent } from './my-account.component';
 import { MyProjectComponent } from './my-project/my-project.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     MyAccountComponent,
@@ -11,7 +12,8 @@ import { MyProjectComponent } from './my-project/my-project.component';
   ],
   imports: [
     CommonModule,
-    MyAccountRoutingModule
+    MyAccountRoutingModule,
+    FormsModule
   ]
 })
 export class MyAccountModule { }
--- src/app/pages/my-account/my-project/my-project.component.ts
+++ src/app/pages/my-account/my-project/my-project.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-project.component.scss']
 })
 export class MyProjectComponent implements OnInit {
+  detail = {
+    category: ['apple','orange'],
+    posttitle: '',
+    postcontent: '',
+    type: '',
+    isShowprice: false,
+    pricelow: 0,
+    pricehight: 1,
+    status: 'review',
+    status_message: ""
+  }
 
   constructor() { }
-
+  
   ngOnInit(): void {
+  }
+
+  selectBox(target: string[], val: string) {
+    var idx = target.indexOf(val);
+    if (idx > -1) target.splice(idx, 1);
+    else target.push(val);
+  }
+
+  submitProject(value: any) {
+    console.log(value)
   }
 
 }
