fc7c49505a10176383cec3900979de3bcef4456d
Reynaldi Julio Setiabudi <reynaldi.setiabudi@jublia.com>
2021-09-22 01:20:37 +0700

    FEATURE(my-account) : Edit profile form added

--- src/app/pages/my-account/my-account.component.ts
+++ src/app/pages/my-account/my-account.component.ts
@@ -6,10 +6,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-account.component.scss']
 })
 export class MyAccountComponent implements OnInit {
-
+  data:any = {
+    "address" : "jl. Mantap Street",
+    "avatar" : "USR8JPSY1589079600000.jpeg",
+    "bio" : "I am Indonesian UI & FrontEnd designer. Lorem ipsum dolor amet",
+    "city" : "kota jakarta timur",
+    "datecreate" : 1611392425493,
+    "dateregister" : 1589079600000,
+    "dateupdate" : 1625762248290,
+    "email" : "john@doe.com",
+    "fullname" : "John Doe",
+    "gender" : "pria",
+    "iduser" : "USR8JPSY1589079600000",
+    "links" : {
+      "link1" : {
+        "icon" : "globe-outline",
+        "name" : "website",
+        "url" : "https://www.johndoe.com/"
+      },
+      "link2" : {
+        "icon" : "bookmark-outline",
+        "name" : "bookmark",
+        "url" : "https://www.johndoe.com/portfolio"
+      },
+      "link3" : {
+        "icon" : "logo-instagram",
+        "name" : "instagram",
+        "url" : "https://www.instagram.com/johndoe"
+      },
+      "link4" : {
+        "icon" : "logo-facebook",
+        "name" : "facebook",
+        "url" : "https://www.facebook.com/johndoe"
+      }
+    },
+    "phoneno" : "08512345678",
+    "phonenoshow" : true,
+    "price" : {
+      "tutor" : 85000
+    },
+    "role" : "freelancer,tutor",
+    "skill" : "software developer",
+    "skills" : "FrontEnd Developer, Coding, Desain",
+    "software" : "Adobe Master, HTML, CSS, JavaSript, Angular",
+    "uid" : "8jpSYFAeQcTcCDtFYKNAcW9pA3j2",
+    "userid" : "johndoe",
+    "_role" : ["freelancer","tutor"]
+  }
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectBox(target: string[], val: string) {
+    var idx = target.indexOf(val);
+    if (idx > -1) target.splice(idx, 1);
+    else target.push(val);
+  }
+
+  register(value: any) {
+    console.log(value)
   }
 
 }
